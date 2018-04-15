749fecd7fc33253ced49abadfa003bc9aab475ee
eo_01@protonmail.com <eo_01@protonmail.com>
2018-04-15 15:13:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 katz_deli = []
 
 take_a_number(katz_deli, "Ada")
-take_a_number(katz_deli, "Ada")
-take_a_number(katz_deli, "Ada")
+take_a_number(katz_deli, "Grace")
+take_a_number(katz_deli, "Kent")
