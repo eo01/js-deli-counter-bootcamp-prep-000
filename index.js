06977ba350cfb0bf5279861279eb3cb32c1023ad
eo_01@protonmail.com <eo_01@protonmail.com>
2018-04-15 15:24:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,19 +1,3 @@
-var katzDeliLine = [];
-
-takeANumber(katzDeliLine, "Ada");
-takeANumber(katzDeliLine, "Grace");
-takeANumber(katzDeliLine, "Kent");
-
-currentLine(katzDeliLine);
-
-nowServing(katzDeliLine);
-
-currentLine(katzDeliLine);
-
-takeANumber(katzDeliLine, "Matz");
-
-currentLine(katzDeliLine);
-
-nowServing(katzDeliLine);
-
-currentLine(katzDeliLine);
+function takeANumber(katzDeliLine,name){
+  
+}
