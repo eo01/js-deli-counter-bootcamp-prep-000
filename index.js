f4f1460fcd41f4a4bdc90a3518546ae1a12fe088
eo_01@protonmail.com <eo_01@protonmail.com>
2018-04-15 15:40:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function takeANumber(katzDeliLine,name){
   var position = katzDeliLine.length+1
   katzDeliLine.push(name)
-  return "Welcome, " + name + ". You are number " + position +"in line."
+  return "Welcome, " + name + ". You are number " + position +" in line."
 }
 function nowServing(deliLine){
   var name;
