0935f674d2a68af220a5ce03a9353353e2d2d2f1
eo_01@protonmail.com <eo_01@protonmail.com>
2018-04-15 15:27:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function takeANumber(katzDeliLine,name){
   var position = katzDeliLine.length+1
   katzDeliLine.push(name)
-  return "Welcome, " + name + "."
+  return "Welcome, " + name + ". You are number " + position +"in line."
 }
