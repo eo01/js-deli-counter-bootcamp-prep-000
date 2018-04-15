9641e112cd8ea454696f1c6940c6d9d5a5232aef
eo_01@protonmail.com <eo_01@protonmail.com>
2018-04-15 15:47:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function currentLine(katzDeliLine){
 var myString = `The line is currently: 1. ${katzDeliLine[0]}`
 
 for(let i = 1; i<katzDeliLine.length;i++){
-  myString += ` ${i+1}. ${katzDeliLine[i]}`
+  myString += `, ${i+1}. ${katzDeliLine[i]}`
 }
 return myString
 }
